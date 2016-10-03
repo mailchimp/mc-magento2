f4ccfdd87c062a381ada413c4b6da6a6aab528f3
gonzalo <gonzalo@ebizmarts.com>
2016-10-03 11:26:19 -0300

    add cron structure

---
+++ Cron/Ecommerce.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * MailChimp Magento Component
+ *
+ * @category Ebizmarts
+ * @package MailChimp
+ * @author Ebizmarts Team <info@ebizmarts.com>
+ * @copyright Ebizmarts (http://ebizmarts.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @date: 10/1/16 10:02 AM
+ * @file: Ecommerce.php
+ */
+namespace Ebizmarts\MailChimp\Cron;
+
+class Ecommerce
+{
+    /**
+     * @var \Magento\Store\Model\StoreManager
+     */
+    private $_storeManager;
+    /**
+     * @var \Ebizmarts\MailChimp\Helper\Data
+     */
+    private $_helper;
+
+    /**
+     * Cron constructor.
+     * @param \Magento\Store\Model\StoreManager $storeManager
+     * @param \Ebizmarts\MailChimp\Helper\Data $helper
+     */
+    public function __construct(
+        \Magento\Store\Model\StoreManager $storeManager,
+        \Ebizmarts\MailChimp\Helper\Data $helper
+    )
+    {
+        $this->_storeManager    = $storeManager;
+        $this->_helper          = $helper;
+    }
+
+    public function execute()
+    {
+        $this->_helper->log(__METHOD__);
+    }
+}
