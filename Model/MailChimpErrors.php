649f2ca0f31c858452577de26206e933bfe10485
gonzalo <gonzalo@ebizmarts.com>
2016-10-27 13:24:08 -0300

    add errors main model

---
+++ Model/MailChimpErrors.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * mc-magento2 Magento Component
+ *
+ * @category Ebizmarts
+ * @package mc-magento2
+ * @author Ebizmarts Team <info@ebizmarts.com>
+ * @copyright Ebizmarts (http://ebizmarts.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @date: 10/21/16 4:55 PM
+ * @file: MailChimpErrors.php
+ */
+
+namespace Ebizmarts\MailChimp\Model;
+
+class MailChimpErrors extends \Magento\Framework\Model\AbstractModel
+{
+    protected function _construct()
+    {
+        $this->_init('\Ebizmarts\MailChimp\Model\ResourceModel\MailChimpErrors');
+    }
+}
