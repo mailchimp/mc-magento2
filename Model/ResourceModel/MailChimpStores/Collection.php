d7552ae8157449674c559679d8d91ca117623f53
gonzalo <gonzalo@ebizmarts.com>
2017-04-21 14:41:14 -0300

    add stores

---
+++ Model/ResourceModel/MailChimpStores/Collection.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * MailChimp Magento Component
+ *
+ * @category Ebizmarts
+ * @package MailChimp
+ * @author Ebizmarts Team <info@ebizmarts.com>
+ * @copyright Ebizmarts (http://ebizmarts.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @date: 3/27/17 1:22 PM
+ * @file: Collection.php
+ */
+namespace Ebizmarts\MailChimp\Model\ResourceModel\MailChimpStores;
+
+class Collection extends \Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult
+{
+    protected function _construct()
+    {
+        $this->_init(
+            'Ebizmarts\MailChimp\Model\MailChimpStores',
+            'Ebizmarts\MailChimp\Model\ResourceModel\MailChimpStores'
+        );
+    }
+}
