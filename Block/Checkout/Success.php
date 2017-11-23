6327fde16ee7ccd7e19b99838bfbc3eaae194301
gonzalo <gonzalo@ebizmarts.com>
2017-11-23 10:19:35 -0300

    retrieve and show the previous selected interest #122

--- Block/Checkout/Success.php
+++ Block/Checkout/Success.php
@@ -22,30 +22,77 @@ class Success extends \Magento\Framework\View\Element\Template
      * @var \Ebizmarts\MailChimp\Helper\Data
      */
     protected $_helper;
+    /**
+     * @var \Magento\Newsletter\Model\SubscriberFactory
+     */
+    protected $_subscriberFactory;
+    /**
+     * @var \Ebizmarts\MailChimp\Model\MailChimpInterestGroupFactory
+     */
+    protected $_interestGroupFactory;
 
     /**
      * Success constructor.
      * @param \Magento\Framework\View\Element\Template\Context $context
      * @param \Magento\Checkout\Model\Session $checkoutSession
      * @param \Ebizmarts\MailChimp\Helper\Data $helper
+     * @param \Magento\Newsletter\Model\SubscriberFactory $subscriberFactory
+     * @param \Ebizmarts\MailChimp\Model\MailChimpInterestGroupFactory $interestGroupFactory
      * @param array $data
      */
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
         \Magento\Checkout\Model\Session $checkoutSession,
         \Ebizmarts\MailChimp\Helper\Data $helper,
+        \Magento\Newsletter\Model\SubscriberFactory $subscriberFactory,
+        \Ebizmarts\MailChimp\Model\MailChimpInterestGroupFactory $interestGroupFactory,
         array $data
     )
     {
         parent::__construct($context, $data);
-        $this->_checkoutSession = $checkoutSession;
-        $this->_helper = $helper;
+        $this->_checkoutSession     = $checkoutSession;
+        $this->_helper              = $helper;
+        $this->_subscriberFactory   = $subscriberFactory;
+        $this->_interestGroupFactory= $interestGroupFactory;
     }
 
     public function getInterest()
     {
         $order = $this->_checkoutSession->getLastRealOrder();
         $interest = $this->_helper->getInterest($order->getStoreId());
+        /**
+         * @var $subscriber \Magento\Newsletter\Model\Subscriber
+         * @var $interestGroup \Ebizmarts\MailChimp\Model\MailChimpInterestGroup
+         */
+        $subscriber = $this->_subscriberFactory->create();
+        $subscriber->loadByEmail($order->getCustomerEmail());
+        $interestGroup = $this->_interestGroupFactory->create();
+        $interestGroup->getBySubscriberIdStoreId($subscriber->getSubscriberId(),$subscriber->getStoreId());
+        $groups = unserialize($interestGroup->getGroupdata());
+        foreach($groups['group'] as $key => $value) {
+            if(isset($interest[$key])) {
+                if(is_array($value)) {
+                    foreach ($value as $groupId) {
+                        foreach ($interest[$key]['category'] as $gkey => $gvalue) {
+                            if ($gvalue['id'] == $groupId) {
+                                $interest[$key]['category'][$gkey]['checked'] = true;
+                            } elseif(!isset($interest[$key]['category'][$gkey]['checked'])) {
+                                $interest[$key]['category'][$gkey]['checked'] = false;
+                            }
+                        }
+                    }
+                } else {
+                    foreach ($interest[$key]['category'] as $gkey => $gvalue) {
+                        if ($gvalue['id'] == $value) {
+                            $interest[$key]['category'][$gkey]['checked'] = true;
+                        } else {
+                            $interest[$key]['category'][$gkey]['checked'] = false;
+                        }
+                    }
+
+                }
+            }
+        }
         return $interest;
     }
     protected function getValues($category)
