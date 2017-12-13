5d2b9725cb95bd066855f9b62f0a2847c3451818
gonzalo <gonzalo@ebizmarts.com>
2017-12-13 16:13:55 -0300

    fix the save of the interest in the customer frontend #122

--- Model/Plugin/Newsletter/Save.php
+++ Model/Plugin/Newsletter/Save.php
@@ -68,22 +68,40 @@ class Save
     public function afterExecute()
     {
         $params = $this->request->getParams();
+
         $subscriber = $this->subscriberFactory->create();
-        $subscriber->loadByCustomerId($this->customerSession->getCustomerId());
         $interestGroup = $this->interestGroupFactory->create();
+        /**
+         * @var $customer \Magento\Customer\Model\Customer
+         */
+        $customer = $this->customerSession->getCustomer();
 
-        $interestGroup->getBySubscriberIdStoreId($subscriber->getSubscriberId(),$subscriber->getStoreId());
-        $interestGroup->setGroupdata(serialize(['group'=>$params['group']]));
-        $interestGroup->setSubscriberId($subscriber->getSubscriberId());
-        $interestGroup->setStoreId($subscriber->getStoreId());
-        $interestGroup->setUpdatedAt($this->date->gmtDate());
-        $interestGroup->getResource()->save($interestGroup);
-        $listId = $this->helper->getGeneralList($subscriber->getStoreId());
-        $sync = $this->helper->getChimpSyncEcommerce($listId,$subscriber->getSubscriberId(),\Ebizmarts\MailChimp\Helper\Data::IS_SUBSCRIBER);
-        if($sync->getId()) {
-            $this->_updateSubscriber($listId, $subscriber->getId(), $this->date->gmtDate(), '', 1);
+        $email = $customer->getEmail();
+
+        try {
+            $subscriber->loadByCustomerId($this->customerSession->getCustomerId());
+            if($subscriber->getEmail()==$email) {
+                $interestGroup->getBySubscriberIdStoreId($subscriber->getSubscriberId(),$subscriber->getStoreId());
+                $interestGroup->setGroupdata(serialize($params));
+                $interestGroup->setSubscriberId($subscriber->getSubscriberId());
+                $interestGroup->setStoreId($subscriber->getStoreId());
+                $interestGroup->setUpdatedAt($this->date->gmtDate());
+                $interestGroup->getResource()->save($interestGroup);
+                $listId = $this->helper->getGeneralList($subscriber->getStoreId());
+                $this->_updateSubscriber($listId, $subscriber->getId(), $this->date->gmtDate(), '', 1);
+            } else {
+                $this->subscriberFactory->create()->subscribe($email);
+                $subscriber->loadByEmail($email);
+                $interestGroup->getBySubscriberIdStoreId($subscriber->getSubscriberId(),$subscriber->getStoreId());
+                $interestGroup->setGroupdata(serialize($params));
+                $interestGroup->setSubscriberId($subscriber->getSubscriberId());
+                $interestGroup->setStoreId($subscriber->getStoreId());
+                $interestGroup->setUpdatedAt($this->date->gmtDate());
+                $interestGroup->getResource()->save($interestGroup);
+            }
+
+        } catch(\Exception $e) {
         }
-
     }
     protected function _updateSubscriber($listId, $entityId, $sync_delta, $sync_error='', $sync_modified=0)
     {
