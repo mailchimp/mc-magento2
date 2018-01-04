1ccb45ebdc6693db61731e6d221dc928a9a1aa97
Logan Stellway <admin@thoughtfully.com>
2018-01-03 19:05:53 -0700

    Adds getUrl function to get the correct URL (fixes issue 164)

--- view/frontend/web/js/mailchimpjs.js
+++ view/frontend/web/js/mailchimpjs.js
@@ -1,6 +1,10 @@
 require(["jquery"], function ($) {
+    function getUrl(path) {
+        return require.toUrl('').split('static/')[0] + path.replace(/^\//g, '');
+    };
+
     $.ajax({
-        url: '/mailchimp/script/get',
+        url: getUrl('/mailchimp/script/get'),
         type: 'POST',
         dataType: 'json',
         showLoader: false
