e8106e26607be548b95a11d028d783355a456a57
Rob Aimes <roba@fisheyehq.com>
2018-04-17 11:04:45 +0100

    Add check before renaming 'address_address1' table column

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -296,28 +296,32 @@ class UpgradeSchema implements UpgradeSchemaInterface
             );
         }
         if (version_compare($context->getVersion(), '1.0.12') < 0) {
-            $connection->changecolumn(
-                $setup->getTable('mailchimp_stores'),
-                'address_address1',
-                'address_address_one',
-                [
-                    'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-                    'length' => 255,
-                    'default' => null,
-                    'comment' => 'first street address'
-                ]
-            );
-            $connection->changecolumn(
-                $setup->getTable('mailchimp_stores'),
-                'address_address2',
-                'address_address_two',
-                [
-                    'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-                    'length' => 255,
-                    'default' => null,
-                    'comment' => 'second street address'
-                ]
-            );
+            if ($connection->tableColumnExists('mailchimp_stores', 'address_address1')) {
+                $connection->changecolumn(
+                    $setup->getTable('mailchimp_stores'),
+                    'address_address1',
+                    'address_address_one',
+                    [
+                        'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                        'length' => 255,
+                        'default' => null,
+                        'comment' => 'first street address'
+                    ]
+                );
+            }
+            if ($connection->tableColumnExists('mailchimp_stores', 'address_address2')) {
+                $connection->changecolumn(
+                    $setup->getTable('mailchimp_stores'),
+                    'address_address2',
+                    'address_address_two',
+                    [
+                        'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                        'length' => 255,
+                        'default' => null,
+                        'comment' => 'second street address'
+                    ]
+                );
+            }
         }
         if (version_compare($context->getVersion(), '1.0.13') < 0) {
             $connection->addColumn(
