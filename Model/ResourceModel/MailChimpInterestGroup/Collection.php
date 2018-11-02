8da5fb197ba38f1830a79dd08f5c093eba72e9d3
gonzalo <gonzalo@ebizmarts.com>
2018-11-02 12:18:56 -0300

    missing file closes #451

---
+++ Model/ResourceModel/MailChimpInterestGroup/Collection.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gonzalo
+ * Date: 10/31/18
+ * Time: 5:58 PM
+ */
+
+namespace Ebizmarts\MailChimp\Model\ResourceModel\MailChimpInterestGroup;
+
+class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
+{
+    protected function _construct()
+    {
+        $this->_init(
+            'Ebizmarts\MailChimp\Model\MailChimpInterestGroup',
+            'Ebizmarts\MailChimp\Model\ResourceModel\MailChimpInterestGroup'
+        );
+    }
+}
