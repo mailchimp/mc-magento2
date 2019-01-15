cbb0d4595a925eb0962a4051a408d0081ebe566a
gonzalo <gonzalo@ebizmarts.com>
2019-01-15 17:51:06 -0300

    remove log closes #554 for magento 2.1

--- Observer/Sales/Order/SubmitAfter.php
+++ Observer/Sales/Order/SubmitAfter.php
@@ -55,7 +55,6 @@ class SubmitAfter implements \Magento\Framework\Event\ObserverInterface
 
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
-        $this->_helper->log(__METHOD__);
         try {
             $this->_cookieManager->deleteCookie(
                 'mailchimp_campaign_id',
