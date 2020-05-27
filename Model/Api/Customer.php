39b32b9ae82c6da9bbfb90b7b9b4a2d1a7bb71fd
gonzalo <gonzalo@ebizmarts.com>
2020-05-27 14:15:58 -0300

    closes #998 for magento 2.2

--- Model/Api/Customer.php
+++ Model/Api/Customer.php
@@ -101,6 +101,7 @@ class Customer
         $collection->getSelect()->limit(self::MAX);
         $counter = 0;
         $customerArray = [];
+        $this->_helper->resetMapFields();
 
         foreach ($collection as $item) {
             $customer = $this->_customerFactory->create();
@@ -152,7 +153,7 @@ class Customer
     protected function buildSubscriberData(\Magento\Customer\Model\Customer $customer)
     {
         $data = [];
-        $data["merge_fields"] = $this->_helper->getMergeVars($customer,$customer->getStoreId());
+        $data["merge_fields"] = $this->_helper->getMergeVars($customer,$customer->getData('store_id'));
         return $data;
     }
     protected function isSubscriber(\Magento\Customer\Model\Customer $customer)
