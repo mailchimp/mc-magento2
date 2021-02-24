594b02fdfc4f096039c183fbfd507d1032783b27
Jeroen <jeroen@reachdigital.nl>
2021-02-24 10:06:56 +0100

    Load subscriber in emulated store

--- Block/Adminhtml/Customer/Edit/Tabs/View/Customer.php
+++ Block/Adminhtml/Customer/Edit/Tabs/View/Customer.php
@@ -10,6 +10,7 @@
  * @date: 11/29/17 2:49 PM
  * @file: Customer.php
  */
+declare(strict_types=1);
 
 namespace Ebizmarts\MailChimp\Block\Adminhtml\Customer\Edit\Tabs\View;
 
@@ -18,23 +19,35 @@ class Customer extends \Magento\Backend\Block\Template
     /**
      * @var \Ebizmarts\MailChimp\Helper\Data
      */
-    protected $helper;
+    private $helper;
+
     /**
      * @var \Magento\Newsletter\Model\SubscriberFactory
      */
-    protected $subscriberFactory;
+    private $subscriberFactory;
 
     /**
      * @var \Magento\Framework\Registry
      */
-    protected $registry;
+    private $registry;
 
     /**
-     * Customer constructor.
+     * @var \Magento\Store\Model\App\Emulation
+     */
+    private $emulation;
+
+    /**
+     * @var \Magento\Customer\Api\CustomerRepositoryInterface
+     */
+    private $customerRepository;
+
+    /**
      * @param \Magento\Backend\Block\Template\Context $context
      * @param \Ebizmarts\MailChimp\Helper\Data $helper
      * @param \Magento\Newsletter\Model\SubscriberFactory $subscriberFactory
      * @param \Magento\Framework\Registry $registry
+     * @param \Magento\Store\Model\App\Emulation $emulation
+     * @param \Magento\Customer\Api\CustomerRepositoryInterface $customerRepository
      * @param array $data
      */
     public function __construct(
@@ -42,20 +55,29 @@ class Customer extends \Magento\Backend\Block\Template
         \Ebizmarts\MailChimp\Helper\Data $helper,
         \Magento\Newsletter\Model\SubscriberFactory $subscriberFactory,
         \Magento\Framework\Registry $registry,
+        \Magento\Store\Model\App\Emulation $emulation,
+        \Magento\Customer\Api\CustomerRepositoryInterface $customerRepository,
         array $data
     ) {
-    
         parent::__construct($context, $data);
-        $this->helper               = $helper;
-        $this->subscriberFactory    = $subscriberFactory;
-        $this->registry             = $registry;
+
+        $this->helper = $helper;
+        $this->subscriberFactory = $subscriberFactory;
+        $this->registry = $registry;
+        $this->emulation = $emulation;
+        $this->customerRepository = $customerRepository;
     }
 
     public function getInterest()
     {
-        $subscriber = $this->subscriberFactory->create();
         $customerId = $this->registry->registry(\Magento\Customer\Controller\RegistryConstants::CURRENT_CUSTOMER_ID);
+        $customerData = $this->customerRepository->getById($customerId);
+        $subscriber = $this->subscriberFactory->create();
+
+        $this->emulation->startEnvironmentEmulation($customerData->getStoreId());
         $subscriber->loadByCustomerId($customerId);
+        $this->emulation->stopEnvironmentEmulation();
+
         return $this->helper->getSubscriberInterest($subscriber->getSubscriberId(), $subscriber->getStoreId());
     }
 }
