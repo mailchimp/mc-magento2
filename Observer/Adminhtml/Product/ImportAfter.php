ac4581082ff2a24768da746fc0ebcfd3bb5d64a4
gonzalezuy <gonzalo@ebizmarts.com>
2021-05-11 16:52:43 -0300

    closes #1167 for magento 2.4

---
+++ Observer/Adminhtml/Product/ImportAfter.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Ebizmarts\MailChimp\Observer\Adminhtml\Product;
+
+use Magento\Framework\Event\Observer;
+
+class ImportAfter implements \Magento\Framework\Event\ObserverInterface
+{
+    /**
+     * @var \Ebizmarts\MailChimp\Helper\Data
+     */
+    protected $helper;
+    protected $productRepository;
+
+    /**
+     * ImportAfter constructor.
+     * @param \Ebizmarts\MailChimp\Helper\Data $helper
+     */
+    public function __construct(
+        \Ebizmarts\MailChimp\Helper\Data $helper,
+        \Magento\Catalog\Model\ProductRepository $productRepository
+    )
+    {
+        $this->helper = $helper;
+        $this->productRepository = $productRepository;
+    }
+    public function execute(Observer $observer)
+    {
+        try {
+            $bunch = $observer->getBunch();
+            foreach ($bunch as $product) {
+                $sku = $product['sku'];
+                $storeId = $product['_store'];
+                $pro = $this->productRepository->get($sku, false,$storeId);
+                $id = $pro->getId();
+                $storeId = $pro->getStoreId();
+                $this->_updateProduct($pro->getStoreId(), $pro->getId(), null,null,1);
+            }
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+    protected function _updateProduct(
+        $storeId,
+        $entityId,
+        $sync_delta = null,
+        $sync_error = null,
+        $sync_modified = null
+    ) {
+        $mailchimpStoreId = $this->helper->getConfigValue(
+            \Ebizmarts\MailChimp\Helper\Data::XML_MAILCHIMP_STORE,
+            $storeId
+        );
+        $this->helper->saveEcommerceData(
+            $mailchimpStoreId,
+            $entityId,
+            \Ebizmarts\MailChimp\Helper\Data::IS_PRODUCT,
+            $sync_delta,
+            $sync_error,
+            $sync_modified
+        );
+    }
+}
