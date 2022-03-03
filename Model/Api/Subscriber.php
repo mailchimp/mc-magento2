b7e9144fd30ac251bfb59112324b8b49abf43ade
gonzalo <gonzalo@ebizmarts.com>
2022-03-03 15:15:20 -0300

    closes #1359 for magento 2.3

--- Model/Api/Subscriber.php
+++ Model/Api/Subscriber.php
@@ -123,7 +123,7 @@ class Subscriber
         if ($mergeVars) {
             $data["merge_fields"] = $mergeVars;
         }
-        $data["status_if_new"] = $this->_getMCStatus($subscriber->getStatus(), $storeId);
+        $data["status_if_new"] = $data["status"] = $this->_getMCStatus($subscriber->getStatus(), $storeId);
         $interest = $this->_getInterest($subscriber);
         if (count($interest)) {
             $data['interests'] = $interest;
