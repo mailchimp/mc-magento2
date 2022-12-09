4056df941b962fdf43acc5fb006224bffe58a1e6
agustinbv <102974092+agustinbv@users.noreply.github.com>
2022-12-09 11:19:34 -0300

    Closes #1627 for 2.3

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -573,7 +573,7 @@ class UpgradeSchema implements UpgradeSchemaInterface
                 ]
             );
         }
-        if (version_compare($context->getVersion(), '100.1.51') < 0 {
+        if (version_compare($context->getVersion(), '102.3.51') < 0 {
             $connection->addColumn(
                 $setup->getTable('mailchimp_sync_batches'),
                 'carts_modified_count',
