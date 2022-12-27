b37d89c31c1dfb27620f594fc36f1e3b2756d8e7
gonzalo <gonzalo@ebizmarts.com>
2022-12-27 12:58:17 -0300

    closes #1640 for magento 2.1

--- Controller/Adminhtml/Ecommerce/ResyncProducts.php
+++ Controller/Adminhtml/Ecommerce/ResyncProducts.php
@@ -57,25 +57,7 @@ class ResyncProducts extends \Magento\Backend\App\Action
         $valid = 1;
         $message = '';
         $params = $this->getRequest()->getParams();
-        if (isset($params['website'])) {
-            $mailchimpStore = $this->helper->getConfigValue(
-                \Ebizmarts\MailChimp\Helper\Data::XML_MAILCHIMP_STORE,
-                $params['website'],
-                'website'
-            );
-        } elseif (isset($params['store'])) {
-            $mailchimpStore = $this->helper->getConfigValue(
-                \Ebizmarts\MailChimp\Helper\Data::XML_MAILCHIMP_STORE,
-                $params['store'],
-                'store'
-            );
-        } else {
-            $mailchimpStore = $this->helper->getConfigValue(
-                \Ebizmarts\MailChimp\Helper\Data::XML_MAILCHIMP_STORE,
-                $this->storeManager->getStore()
-            );
-        }
-
+        $mailchimpStore = $params['mailchimpStoreId'];
         $resultJson = $this->resultJsonFactory->create();
         try {
             $this->helper->resyncProducts($mailchimpStore);
