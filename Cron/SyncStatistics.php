aaacc0a1dc37df5acd9d16c16d7e3bc6cf70078e
gonzalo <gonzalo@ebizmarts.com>
2025-11-05 14:30:18 -0300

    closes #2210  for magento 2.4

--- Cron/SyncStatistics.php
+++ Cron/SyncStatistics.php
@@ -71,9 +71,11 @@ class SyncStatistics
             $res = json_decode($response, true);
             if (key_exists('error',$res) && $res['error']) {
                 $this->helper->log("Something went wrong while syncing statistics");
+                $this->cleanData();
                 return;
             } elseif (key_exists('enabled',$res) && !$res['enabled']) {
                 $this->helper->log("You are not authorized to sync statistics");
+                $this->cleanData();
                 return;
             }
             $this->helper->log("Processing sync statistics");
